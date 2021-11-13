bbced92f5eec39ef74eec9d645433e152936d539
qhuy1911 <qhuy1911@gmail.com>
2021-11-13 15:38:15 +0700

    Add morgan

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/news', (req, res) => {
+app.use(morgan('combined'))
+
+app.get('/home', (req, res) => {
     var a = 1
     var b = 2
     var c = a + b
