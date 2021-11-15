e96ed4254731d56d182011c1d0ddbe30d1a2a6e2
qhuy1911 <qhuy1911@gmail.com>
2021-11-15 22:39:19 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -9,18 +9,23 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // Http logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
@@ -28,5 +33,5 @@ route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
