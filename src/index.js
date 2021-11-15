548cb3f9fb0369f19f82abff0a7322ec82a8af59
qhuy1911 <qhuy1911@gmail.com>
2021-11-15 16:06:32 +0700

    Reactor code: create routes, app/controller folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // Http logger
 // app.use(morgan('combined'));
@@ -17,18 +24,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    console.log(req.query.q)
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
+route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
